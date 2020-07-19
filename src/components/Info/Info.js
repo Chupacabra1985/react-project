a893306a169220a969af338a1d91bcc95a4ed516
Jan Roztropinski <jan.roztropinski@twino.pl>
2020-07-20 00:47:49 +0200

    feature(task) Rozwinięcie podstron

---
+++ src/components/Info/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '../Container/Container';
+import Hero from '../Hero/Hero';
+import {settings} from '../../data/dataStore';
+
+const Info = () => (
+  <Container>
+    <Hero titleText={settings.info.title} image={settings.info.image}/>
+    <p>{settings.info.content}</p>
+  </Container>
+);
+
+export default Info;
--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -10,6 +10,16 @@ export const settings = {
     defaultText: 'Search...',
     icon: 'search',
   },
+  info: {
+    title: 'Info',
+    image: '../images/info.jpg',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  faq: {
+    title: 'FAQ',
+    image: '../images/PMS-FAQ.png',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
   defaultListDescription: '<p>I can do all the things!!!</p>',
   defaultColumnIcon: 'list-alt',
 };
